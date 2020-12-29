2ede88875731929cdf3a79401252684765f45b2e
labadmin <patrickcberry@gmail.com>
2020-12-28 21:57:34 -0800

    Updates

--- lab-influxdb/init.sh
+++ lab-influxdb/init.sh
@@ -4,5 +4,5 @@ docker run --rm \
       -e INFLUXDB_DB=db0 \
       -e INFLUXDB_ADMIN_USER=admin -e INFLUXDB_ADMIN_PASSWORD=password \
       -e INFLUXDB_USER=telegraf -e INFLUXDB_USER_PASSWORD=password \
-      -v /home/labadmin/influxdb-data:/var/lib/influxdb \
+      -v /home/labadmin/data-influxdb:/var/lib/influxdb \
       influxdb /init-influxdb.sh
