e11ac68454beb9af8ec5d00fd8740d7eedb9f0d2
root <patrickcberry@gmail.com>
2021-05-17 11:01:35 +0000

    update

--- lab-web/deploy.sh
+++ lab-web/deploy.sh
@@ -3,4 +3,4 @@
  git pull
  docker stop labweb
  docker build -t lab-web .
- docker run -it -d -p 8080:80 --rm --restart unless-stopped --name labweb lab-web
+ docker run -it -d -p 8080:80 --restart unless-stopped --name labweb lab-web
